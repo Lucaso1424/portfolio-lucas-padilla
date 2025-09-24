d6f48c7d6dd2140edd85984cd5c1133d1b361148
Lucaso1424 <lucaspadilla648@gmail.com>
2025-09-24 11:44:30 +0200

    jorel.ts fonts added to src/fonts

---
+++ src/fonts/jorel.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const jorelFont = localFont({
+  src: "../fonts/JorelMinesweeperPlus-Regular.otf",
+  display: "swap",
+  variable: "--font-jorel",
+});
